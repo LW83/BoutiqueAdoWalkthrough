c96b41b87d8bcbf070ea0a5d6d871a57727f88b3
LW83 <ljbwilkinson@gmail.com>
2023-02-02 17:44:16 +0000

    Added sorting

--- products/views.py
+++ products/views.py
@@ -12,8 +12,23 @@ def all_products(request):
     products = Product.objects.all()
     query = None  # To start page without an error that no search term has been entered
     categories = None
+    sort = None # Need to ensure defined in order to return template properly when not using any sorting. 
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')  # if it exists splitting into list at the comma
             products = products.filter(category__name__in=categories)  # doubleunderscore syntax used her means looking for name field of category model can do here as category and product model linked by FK
@@ -28,10 +43,13 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)  # Used to query on OR basis, i before contains makes them case insenstive
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)  # context added as will need to send some things back to the template
