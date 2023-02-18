e5013cc4ab3f31bfbb1ede24b0c53464f707251b
LW83 <ljbwilkinson@gmail.com>
2023-02-18 17:26:47 +0000

    Profiles part 4 - added order histroy and updated checkout views

--- checkout/admin.py
+++ checkout/admin.py
@@ -17,7 +17,7 @@ class OrderAdmin(admin.ModelAdmin):
                        'stripe_pid',)
 
     # Allows us to specify order of fields in Admin interface
-    fields = ('order_number', 'date', 'full_name',
+    fields = ('order_number', 'user_profile', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city',
               'street_address1', 'street_address2',
